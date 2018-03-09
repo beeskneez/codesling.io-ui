8eabbbad6267755bebeaa7580d13c798a31349ef
downier <raulishere@gmail.com>
2018-03-08 19:02:29 -0800

    (dev) Moving components

--- client/src/components/Friends/index.jsx
+++ client/src/components/Friends/index.jsx
@@ -1,47 +1,57 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Logo from "../globals/Logo";
+import Input from "../globals/forms/Input";
+import Button from "../globals/Button/";
 import { FriendsList } from "./FriendsList.jsx";
 
 class Friends extends Component {
   state = {
-    friends: [],
+    friends: []
   };
 
   async componentWillMount() {
     const id = localStorage.getItem("id");
-    const { data } = await axios.get(`http://localhost:3396/api/friends/fetchAllFriends/${id}`);
+    const { data } = await axios.get(
+      `http://localhost:3396/api/friends/fetchAllFriends/${id}`
+    );
     this.setState({ friends: data });
-    console.log(this.state.friends);
-
   }
-  
+
   deleteFriend(e) {
-    
+    console.log(e)
   }
 
   addFriend(e) {
-
+    console.log(this.state.friend)
   }
 
-  fetchFriends = async () => {
-    const id = localStorage.getItem("id");
-    const { data } = await axios.get(`http://localhost:3396/api/friends/fetchAllFriends/${id}`);
-    this.setState({ friends: data });
-  }
+  handleFriendInput = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
 
   render() {
-    
-    return (
-      <div className="landing-page-container">
+    return <div className="landing-page-container">
         <Logo className="landing-page-logo" />
         <br />
         <h5>Friends:</h5>
-        {this.state.friends.map((user) => 
-          <div>{user.username}</div>
-        )}
-      </div>
-    )
+        <FriendsList friends={this.state.friends} deleteFriend={() => this.deleteFriend} />
+        
+        {this.state.friends.map((user, index) => <div>
+            <div key={index} name={user.user_id}>
+              {user.username}
+            </div>
+            <a onClick={e => this.deleteFriend(this.props.name)}>
+              {" "}
+              Remove{" "}
+            </a>
+          </div>)}
+        <br />
+        <br />
+        <Input name="friend" type="friend" placeholder="Add a friend" onChange={this.handleFriendInput} />
+        <Button backgroundColor="red" color="white" text="Add" onClick={e => this.addFriend(e)} />
+      </div>;
   }
 }
 
