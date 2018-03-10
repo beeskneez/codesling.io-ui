69b780095fc68b61557f3c776ad092bd7c49cc60
downier <raulishere@gmail.com>
2018-03-09 17:07:02 -0800

    (dev) Timer running on nav

--- client/src/components/Sling/EditorHeader/EditorNavbar.js
+++ client/src/components/Sling/EditorHeader/EditorNavbar.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const EditorNavbar = () => (
-  <nav className="editor-navbar">
-    <ul>
-      <li>Item 1</li>
-      <li>Item 2</li>
-      <li>Item 3</li>
-    </ul>
-  </nav>
-);
+class EditorNavbar extends Component {
+  state = {
+    secondsElapsed: 0,
+    laps: [],
+  }
+
+  componentDidMount() {
+    this.interval();
+  }
+
+  interval = () => {
+    setInterval( () =>
+      this.setState({
+        secondsElapsed: this.state.secondsElapsed + 1
+      })
+    , 1000);
+  }
+
+  formattedSeconds = (sec) => {
+    return Math.floor(sec / 60) +
+      ':' +
+    ('0' + sec % 60).slice(-2)
+  }
+
+  logout = () => {
+    console.log('logout buttonzzzz')
+  }
+
+  render() {
+    return (
+      <nav className="editor-navbar">
+        <ul>
+          
+          <li>{this.formattedSeconds(this.state.secondsElapsed)}</li>
+          <li>History</li>
+          <li>Logout</li>
+        </ul>
+      </nav>
+    );
+  }
+}
+  
 
 export default EditorNavbar;
